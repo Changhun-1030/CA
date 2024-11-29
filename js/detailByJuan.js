0ea14dabb28118b8c3366b51b9eb4f762c43989a
yuyuane <yuyuane@163.com>
2024-11-29 21:36:29 +0800

    1

---
+++ js/detailByJuan.js
@@ -0,0 +1,12 @@
+
+function clickToContact(){
+    location.href="./contact.html";
+}
+
+$(document).ready(function(){
+    $("#doSubmit").click(function(){
+        var keyword = $("#inputPassword2").val();
+        
+    });
+    
+});
