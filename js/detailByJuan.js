691da75a0c7c32efc34e19951dccdfb479b09e44
yuyuane <yuyuane@163.com>
2024-12-02 21:07:25 +0800

    1

--- js/detailByJuan.js
+++ js/detailByJuan.js
@@ -1,12 +1,16 @@
 
-function clickToContact(){
-    location.href="./contact.html";
+function clickToContact() {
+    location.href = "./contact.html";
 }
 
-$(document).ready(function(){
-    $("#doSubmit").click(function(){
+$(document).ready(function () {
+    $("#doSubmit").click(function () {
         var keyword = $("#inputPassword2").val();
-        
+        if (keyword == "") {
+            alert("Please enter keyword!");
+        } else {
+            location.href = "https://www.ireland.com/en-gb/search/?term=" + keyword;
+        }
     });
-    
+
 });
